1acba1747163bbc504628d0580b7a097acf46f89
Lucas Fernandes <lucasmatdlx@hotmail.com>
2019-09-22 02:14:02 -0300

    Adidas Bot automation for australian version
    Adidas Bot automation for australian version
    First you need to pip install selenium
    second you need to download chromewebdriver and put it into your adidas folder
    Then just run your file on vscode terminal or your own terminal and be happy

---
+++ Adidas/config (1).py
@@ -0,0 +1,16 @@
+keys = {
+        "product_url": "https://www.adidas.com.au/run-belt/CF5210.html",
+        "name": "Gilmar",
+        "lastname" : "Ferrini",
+        "company_name": "NomeEmpresarial",
+        "email": "email@gmail.com",
+        "phone_number": "6789998212",
+        "street_address": "Forest Hills Dr.",
+        "suburb": "Haymarket",
+        "apt": "666",
+        "street_no": "NEW",
+        #"city": "Timbuktu", this is autofilled
+        "zip_code": "2000",
+        "card_cvv": "666",
+        "card_number": "1228199669918221"
+}
---
+++ Adidas/inicial (1).py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from config import keys
+import time
+
+def timeme(method):
+    def wrapper(*args, **kw):
+        startTime = int(round(time.time() * 1000))
+        result = method(*args, **kw)
+        endTime = int(round(time.time() * 1000))
+        print((endTime - startTime)/1000, 's')
+        return result
+    return wrapper
+
+
+
+driver = webdriver.Chrome('chromedriver/chromedriver')
+driver.get(keys['product_url'])
+#proposal_origin__flA-p gl-link
+time.sleep(.10)
+
+driver.find_element_by_xpath('/html/body/div[3]/div[1]/div/div/div/div/div[2]/a/strong').click() # OPCAO BR E AU
+#driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div/div[1]/div/div/div/ol/li[2]/a').click()  #HOME
+driver.find_element_by_css_selector('.gl-cta.gl-cta--primary.gl-cta--full-width.btn-bag').click() # click add to bag
+time.sleep(5)
+driver.refresh()
+#click sacola
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/div[3]/div/div[2]/div[2]/a').click() # sacola
+time.sleep(2)
+driver.find_element_by_xpath('/html/body/div[1]/div[5]/div/div[1]/div[1]/div[1]/div[2]/div[1]/form/fieldset/button').click() #checkout
+
+
+#form
+
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[1]/div/input').send_keys(keys['name'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[2]/div/input').send_keys(keys['lastname'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[3]/div/input').send_keys(keys['company_name'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[4]/div/input').send_keys(keys['apt'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[5]/div/input').send_keys(keys['street_no'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[6]/div/input').send_keys(keys['street_address']) 
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[7]/div/input').send_keys(keys['suburb'])
+
+
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[9]/div[1]/input').send_keys(keys['zip_code'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[1]/div/fieldset/div/div[1]/div[11]/div[1]/input').send_keys(keys['phone_number'])
+
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[2]/div[2]/div[2]/div/input').send_keys(keys['phone_number'])
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[2]/div[2]/div[1]/div/input').send_keys(keys['email'])
+
+#review & pay
+time.sleep(1)
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/form/div[2]/div[2]/div[4]/div[1]/button[1]').click()
